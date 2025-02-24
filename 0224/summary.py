5224170a59f74482093252a545044089b3c3968a
PAUL <kimjs20020426@gmail.com>
2025-02-24 17:00:12 +0900

    update

---
+++ 0224/summary.py
@@ -0,0 +1,12 @@
+# numpy
+- 과학 연산을 위한 python 패키지 중 하나.
+  NumPy는 "Numeric Python"의 약자이며 대규모 다차원 배열과 행렬 연산에 필요한 다양한 함수와 메소드를 제공.
+
+## ndarray
+- numpy의 N차원 배열 객체. python의 리스트와 달리 한 가지 유형의 자료만 입력할 수 있음.
+- 생성 방법
+    a = [1, 2, 3, 4, 5]
+    a_ndarray = np.array(a, int)
+
+# pandas
+- DataFrame: DataFrame 객체는 행과 열로 이루어진 2차원 데이터를 다루기 위한 객체

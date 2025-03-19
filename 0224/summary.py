c499f1f98f86f296adadbf542069f669761ce942
PAUL <kimjs20020426@gmail.com>
2025-03-19 11:33:32 +0900

    Django_review

--- 0224/summary.py
+++ 0224/summary.py
@@ -5,8 +5,8 @@
 ## ndarray
 - numpy의 N차원 배열 객체. python의 리스트와 달리 한 가지 유형의 자료만 입력할 수 있음.
 - 생성 방법
-    a = [1, 2, 3, 4, 5]
-    a_ndarray = np.array(a, int)
+a = [1, 2, 3, 4, 5]
+a_ndarray = np.array(a, int)
 
 # pandas
 - DataFrame: DataFrame 객체는 행과 열로 이루어진 2차원 데이터를 다루기 위한 객체
